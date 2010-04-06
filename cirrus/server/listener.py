420d52fbb9bf9b6e99b0b570e5cd9281ea736007
James Labocki <jameslabocki@gmail.com>
2010-04-06 14:32:50 -0400

    Adding initial files

---
+++ cirrus/server/listener.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import logging
+import socket
+import os
+
+def start_server():
+	print "Starting Server"
+	logging.info("Starting Server")
---
+++ server.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from cirrus.server import listener
+
+if __name__ == '__main__':
+	listener.start_server()
