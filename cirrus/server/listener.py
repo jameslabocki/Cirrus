b1fb6f681a1e7f1c86098314b8ddc509cf5b555a
James Labocki <jlabocki@redhat.com>
2010-05-24 13:59:46 -0400

    Removing

--- cirrus/server/listener.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import logging
-import socket
-import os
-
-def start_server():
-	print "Starting Server"
-	logging.info("Starting Server")
--- server.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from cirrus.server import listener
-
-if __name__ == '__main__':
-	listener.start_server()
