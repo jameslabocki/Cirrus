1edd70f8a7a46a0a4d8dac6e24afd910ba2ca5f4
cirrus <cirrus@gambit.localdomain>
2010-05-24 20:36:25 -0400

    Added Argument Handling

--- bin/Cirrus.sh
+++ bin/Cirrus.sh
@@ -11,13 +11,57 @@
 # remove - Remove a virtual machine from the service
 # list - List virtual machines in a service
 
-# Error Checking
+#Error Checking
 if [ $# -lt 2 ]; then
 	echo "./Cirrus.sh service add|remove|list [OPTIONS]"
+	exit 1
 fi
 
-# Source Configuration File
-/usr/local/Cirrus/Cirrus/conf/Cirrus.conf
+while [ $# -gt 0 ]
+do
+	case $1
+	in
+		-c)
+		config=$2
+		shift 2
+		;;
 
+		service)
+		service=service
+		shift 2
+		;;
+
+                add)
+                add=add
+                shift 2
+                ;;
+
+                remove)
+                remove=remove
+                shift 2
+                ;;
+
+                list)
+                list=list
+                shift 2
+                ;;
+
+
+    *)
+	echo "The arguments to use are"
+	echo "-c: configuration file"
+	echo "add - Add a virtual machine to the service"
+	echo "remove - Remove a virtual machine from the service"
+	echo "list - List virtual machines in a service"
+      shift 1
+    ;;
+  esac
+done
+
+# Source Configuration File if none specified
+if [ -z $config ]; then
+	config=/usr/local/Cirrus/Cirrus/conf/Cirrus.conf
+fi
 
 #
+echo "using $config"
