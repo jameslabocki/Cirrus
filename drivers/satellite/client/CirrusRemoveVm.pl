d31d394d00c12e7e48346a45b8657c7af1d0f5ac
James Labocki <jlabocki@redhat.com>
2010-06-14 09:10:09 -0400

    Added Removal Script

--- drivers/satellite/client/CirrusRemoveVm.pl
+++ drivers/satellite/client/CirrusRemoveVm.pl
@@ -1,36 +1,33 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
+#use strict;
+#use warnings;
 use Frontier::Client;
 
 #my $HOST = '';
 #my $user = '';
 #my $pass = '';
 my $HOST = 'ra-sat-vm.ra.rh.com';
-my $user = 'admin';
+my $user = 'tenant';
 my $pass = '100yard-';
 my @newList;
-my @test;
-my @id;
+my $system = $ARGV[0];
 
 my $client = new Frontier::Client(url => "http://$HOST/rpc/api");
 my $session = $client->call('auth.login', $user, $pass);
 
-my $lastsystem = $newList[-1];
+my $systems = $client->call('system.listUserSystems', $session);
+#foreach my $system (@$systems) {
+#		print $system->{'name'}."\n";
+#}
 
-#print "\nGet Id\n";
-my $details = $client->call('system.getId', $session, $ARGV[0]);
-foreach my $detail (@$details) {
-	print $detail->{'id'}."\n";
-	my $systemId = $detail->{'id'};
-	push(@id,$systemId);
+
+my $details = $client->call('system.getId', $session, $system);
+foreach my $details (@$details) {
+#	print $details->{'id'}."\n";
+	my $id = $details->{'id'};
+	my $delete = $client->call('system.deleteSystems', $session, $id);
 }
 
-#print "\nPrint ID of last\n";
-#print $id[-1]."\n";
-my $lastid = $id[-1];
-
-#print "\nDelete Last ID";
-my $delete = $client->call('system.deleteSystems', $session, $lastid);
-
+#log out, it's polite
+$client->call('auth.logout', $session);
